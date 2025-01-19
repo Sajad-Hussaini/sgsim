b4a9a9a0aa6ef7b96d5e218fb4cea56caa5035ff
sajad-hussaini <hussaini.smsajad@gmail.com>
2025-01-19 11:09:03 +0000

    lazy import

--- SGSIM/__init__.py
+++ SGSIM/__init__.py
@@ -1,13 +1,22 @@
 """
 SGSIM package initialization.
-
-This package includes modules for stochastic modeling, (ground) motion modeling,
-optimization, and visualization.
 """
 
-from .core.stochastic_model import StochasticModel
-from .motion.motion_model import Motion
-from .optimization.model_calibrate import calibrate
-from .visualization.model_plot import ModelPlot
+import importlib
 
 __all__ = ['StochasticModel', 'Motion', 'calibrate', 'ModelPlot']
+
+module_map = {
+    'StochasticModel': 'core.stochastic_model',
+    'Motion': 'motion.motion_model',
+    'calibrate': 'optimization.model_calibrate',
+    'ModelPlot': 'visualization.model_plot',
+    'signal_analysis': 'motion.signal_analysis',
+    }
+
+def __getattr__(name):
+    # Dynamically import and return the requested module attribute
+    if name in module_map:
+        module = importlib.import_module(f'.{module_map[name]}', package=__name__)
+        return getattr(module, name)
+    raise AttributeError(f"module {__name__} has no attribute {name}")
