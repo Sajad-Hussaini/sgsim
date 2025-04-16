c6e938b47d516b19666e7612a0c5f40cf46c5018
Sajad Hussaini <hussaini.smsajad@gmail.com>
2025-04-16 14:02:19 +0100

    ready for pypy v1.0.61

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="sgsim",
-    version="1.0.0",
+    version="1.0.61",
     author="S.M.Sajad Hussaini",
     author_email="hussaini.smsajad@gmail.com",
-    description="SGSIM: stochastic ground motion simulation model.",
+    description="SGSIM: Stochastic Ground-motion SIMulation model.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Sajad-Hussaini/SGSIM",
+    url="https://github.com/Sajad-Hussaini/sgsim",
+    license="AGPL-3.0",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=['numpy', 'scipy', 'numba', 'matplotlib'],
-    keywords=['Python', 'SGSIM', 'simulation model', 'stochastic ground motion'],
+    keywords=['Python', 'SGSIM', 'stochastic ground motion simulation', 'earthquake engineering'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: GNU AGPL v3",
         "Operating System :: OS Independent",
-        "Intended Audience :: Science/Engineering/Research"]
+        "Intended Audience :: Science/Engineering/Research",
+        "Topic :: Scientific/Engineering",]
     )
